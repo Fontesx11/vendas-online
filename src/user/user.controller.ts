38b998606af6cf8795d62e4421d805602d0b6b3b
Fontesx11 <fontesdgfranca@gmail.com>
2025-04-21 19:20:23 -0300

    feat: geat all users (GET)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
+import { UserService } from './user.service';
+import { User } from './interface/user.interface';
 
 @Controller('user')
 export class UserController {
-  @Post()
-  async createUser(@Body() createUser: CreateUserDto){
 
-    return {
-      ...createUser,
-      password: undefined,
-    };
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto): Promise<User>{
+
+    return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  async getAllUsers(): Promise<User[]>{
+    return this.userService.getAllUsers();
   }
 }
