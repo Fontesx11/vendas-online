5c97d6051fcc6bf3234ee1a79a7ea261d24be736
root <root@SENAI-D-183665.SISTEMAFIEPE.LOCAL>
2025-05-07 12:57:30 -0300

    feat: create DTO using pipe

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
+import { ReturnUserDto } from './dto/returnUser.dto';
 
 @Controller('user')
 export class UserController {
 
   constructor(private readonly userService: UserService) {}
 
+  @UsePipes(ValidationPipe)
   @Post()
   async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity>{
 
@@ -15,7 +17,7 @@ export class UserController {
   }
 
   @Get()
-  async getAllUsers(): Promise<UserEntity[]>{
-    return this.userService.getAllUsers();
+  async getAllUsers(): Promise<ReturnUserDto[]>{
+    return (await this.userService.getAllUsers()).map((userEntity) => new ReturnUserDto(userEntity));
   }
 }
