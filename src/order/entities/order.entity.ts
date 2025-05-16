2627cf9ee6a63ded8332779c67c1499c398d8357
Fontesx11 <fontesdgfranca@gmail.com>
2025-05-16 11:29:13 -0300

    feat: creating relations

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,7 +1,15 @@
+import { AddressEntity } from 'src/address/entities/address.entity';
+import { OrderProductEntity } from 'src/order-product/entities/oder-product.entity';
+import { PaymentEntity } from 'src/payment/entities/payment.entity';
+import { UserEntity } from 'src/user/entities/user.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -23,9 +31,18 @@ export class OrderEntity {
   @Column({ name: 'payment_id', nullable: false })
   paymentId: number;
 
-  @CreateDateColumn({ name: 'created_at' })
-  createdAt: Date;
+  @ManyToMany(() => UserEntity, (user) => user.orders)
+  @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
+  user?: UserEntity;
 
-  @UpdateDateColumn({ name: 'updated_at' })
-  updatedAt: Date;
+  @ManyToMany(() => AddressEntity, (address) => address.orders)
+  @JoinColumn({ name: 'address_id', referencedColumnName: 'id' })
+  address?: AddressEntity;
+
+  @ManyToMany(() => PaymentEntity, (payment) => payment.orders)
+  @JoinColumn({ name: 'payment_id', referencedColumnName: 'id' })
+  payment?: PaymentEntity;
+
+  @OneToMany(() => OrderProductEntity, (orderProduct) => orderProduct.order)
+  ordersProduct?: OrderProductEntity[];
 }
